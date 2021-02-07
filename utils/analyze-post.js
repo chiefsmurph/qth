7aee1ea20315294d36fefb61fd2ba1dbeea866c0
chiefsmurph <chiefsmurph@gmail.com>
2021-02-06 19:27:35 -0600

    get model rudimentary

--- utils/analyze-post.js
+++ utils/analyze-post.js
@@ -29,6 +29,8 @@ const getIndexValues = function (str, regex) {
 };
 
 
+
+
 const getPrice = ({ title, description }) => {
     const str = JSON.stringify({ title, description });
     // const matches = str.match();
@@ -52,14 +54,13 @@ const getPrice = ({ title, description }) => {
         ).reverse()
     ).flat();
 
-    if (title.includes('MOTO')) {
-
-        console.log(JSON.stringify({
-            post: { title, description },
-            possiblePricesMatches,
-            anchorMatches
-        }, null, 2));
-    }
+    // if (title.includes('MOTO')) {
+    //     console.log(JSON.stringify({
+    //         post: { title, description },
+    //         possiblePricesMatches,
+    //         anchorMatches
+    //     }, null, 2));
+    // }
 
     const firstAnchorIndex = anchorMatches[0];
     if (!firstAnchorIndex) return;
@@ -74,11 +75,49 @@ const getPrice = ({ title, description }) => {
     return Number(analyzedPriceMatches[0].text);
 };
 
+
+
+const getModel = ({ title, description }) => {
+    const str = JSON.stringify({ title, description });
+    const makes = [
+        'Yaesu',
+        'Icom',
+        'Palstar',
+        'LNR',
+        'Elecraft',
+        'Kenwood',
+        'Xiegu',
+        'IC',
+        'FT',
+    ];
+
+    const makesMatches = makes.map(make => getIndexValues(str, new RegExp(make, 'gi'))).flat().sort((a, b) => a.index - b.index);
+    if (!makesMatches.length) return;
+    const [{ text: makeMatch, index, endIndex }] = makesMatches;
+    const substr = str.substring(endIndex + 1);
+
+    const modelGuess = substr.split(' ')[0].trim();
+    const myGuess = [makeMatch, modelGuess].join(' ');
+
+    // console.log({ makesMatches})
+
+    console.log(JSON.stringify({
+        str,
+        makeMatch,
+        substr,
+        myGuess
+    }, null, 2));
+    return myGuess;
+};
+
+
+
 module.exports = post => {
 
     return {
         ...post,
         isWtb: isWtb(post),
-        price: getPrice(post)
+        price: getPrice(post),
+        model: getModel(post)
     }
 };
